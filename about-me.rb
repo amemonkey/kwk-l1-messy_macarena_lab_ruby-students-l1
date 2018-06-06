310f3b79f0b622393128b7275c2ecd8ac23245c5
amemonkey <ameliafunaki789@gmail.com>
2018-06-06 21:55:11 +0000

    automatically backed up by learn

--- about-me.rb
+++ about-me.rb
@@ -4,12 +4,11 @@
   puts "I am from Utah"
   puts "My favorite food is burgers"
   puts "I like to play volleyball"
-  puts "Reading is my favorite past time"
-  
+ 
+ def greeting puts "Reading is my favorite past time"
   local_greeting = "Hey DUDeee"
   your_name = "Randi"
   my_name = "Ame"
   the_time = "summertime"
-  def greeting
     puts "#{local_greeting} #{your_name}, I'm #{my_name}. How's your #{time_of_day}?"
   end
