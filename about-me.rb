51997a422d36a01e00997b597afda2a82f7a0ca1
amemonkey <ameliafunaki789@gmail.com>
2018-06-06 21:56:39 +0000

    Done.

--- about-me.rb
+++ about-me.rb
@@ -1,15 +1,18 @@
-def about_me
+
   puts "My name is Ame"
   puts "I am 13 years old"
   puts "I am from Utah"
   puts "My favorite food is burgers"
   puts "I like to play volleyball"
-  puts "Reading is my favorite past time"
-  
+ puts "Reading is my favorite past time"
+ 
+ def greeting 
   local_greeting = "Hey DUDeee"
   your_name = "Randi"
   my_name = "Ame"
   the_time = "summertime"
-  def greeting
     puts "#{local_greeting} #{your_name}, I'm #{my_name}. How's your #{time_of_day}?"
   end
+end
+
+greeting
